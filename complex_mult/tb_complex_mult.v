4f2c256b5fb7858ddb38348e9ac2b93e9efb0733
OldChen <chen_zhongyao@163.com>
2017-09-25 20:58:20 +0800

    update

--- complex_mult/tb_complex_mult.v
+++ complex_mult/tb_complex_mult.v
@@ -16,7 +16,8 @@ wire p_valid;
 wire signed [N+N:0] pr,pi;
 //  Simulation Variables
 
-reg [4*N-1:0] i;
+reg [4*N-1:0] i,j;
+reg signed [N-1:0] exp_ar,exp_ai,exp_br,exp_bi;
 reg signed [N+N:0] real_part_dsp,imag_part_dsp;
 
 // Instantiate the Unit Under Test (UUT)
@@ -43,47 +44,59 @@ initial begin
 
     clk = 0;
     i = 0;
+    j = 0;
     ab_valid = 0;
     ar = 0; ai = 0; br = 0; bi = 0;
-    real_part_dsp = 0; imag_part_dsp = 0;
 
     #200;
 
     // Add stimulus here
     $display("Simulation begin!");
 
-    for(i = 0; i < 5/*2**(4*N)*/; i = i + 1) begin
+    for(i = 0; i < 2**(4*N)-1; i = i + 1) begin
         @(posedge clk)
-        #1 ab_valid = 1;
+        #1; // !! very important otherwise iverilog simulate goes wrong!
+        ab_valid = 1;
         ar = i[N-1:0];
         ai = i[2*N-1:N];
         br = i[3*N-1:2*N];
         bi = i[4*N-1:3*N];
-
-        real_part_dsp = ar*br - ai*bi;
-        imag_part_dsp = ai*br + ar*bi;
-
-        @(posedge clk)
-        #1 ab_valid = 0;
-        ar = 0; ai = 0; br = 0; bi = 0;
-
-        @(posedge p_valid)
-        if( (pr != real_part_dsp)&&(pi != imag_part_dsp) ) begin
-            $display(" Fail - Module output does not equal expected value\n");
-            $stop;
-        end
-
     end
     @(posedge clk)
-    #1 ab_valid = 0;
+    ab_valid = 0;
     ar = 0; ai = 0; br = 0; bi = 0;
 
-    @(posedge clk)
+    @(negedge p_valid)
+    #100;
 
     $display("All tests are passed!");
 
     $finish;
+
 end
+
+initial begin
+	while(1)begin
+		@(posedge clk);
+		if(p_valid)begin
+			exp_ar = j[N-1:0];
+	        exp_ai = j[2*N-1:N];
+	        exp_br = j[3*N-1:2*N];
+	        exp_bi = j[4*N-1:3*N];
+
+	        real_part_dsp = exp_ar*exp_br - exp_ai*exp_bi;
+	        imag_part_dsp = exp_ai*exp_br + exp_ar*exp_bi;
+	        if( (pr != real_part_dsp)&&(pi != imag_part_dsp) ) begin
+	            $display(" Fail - Module output does not equal expected value\n");
+	            $display("%d,%d,%d,%d",pr,pi,real_part_dsp,imag_part_dsp);
+	            $stop;
+	        end
+
+			j = j + 1;
+		end
+	end
+end
+
 
 ///////////////////////////////////////////////////////////////////////////////
 
